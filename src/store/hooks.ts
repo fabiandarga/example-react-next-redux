924c87c2c0eea57f43f91c3ce25e4fa32952256e
Fabian Darga <kontakt@fabiandarga.de>
2024-12-04 10:55:09 +0100

    fix typing of store hooks

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,6 @@
-import { useDispatch, useSelector, useStore } from 'react-redux'
+import { useDispatch, UseSelector, useSelector, useStore } from 'react-redux'
 import type { AppDispatch, AppStore, RootState } from './store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector = useSelector<RootState>;
+export const useAppSelector: UseSelector<RootState> = useSelector;
